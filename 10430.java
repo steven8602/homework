bc9e5895604a4fbe1ffe88d2339a11215dc47ac2
이신우 <51151313+steven8602@users.noreply.github.com>
2022-03-17 20:19:35 +0900

    Update 10430.java

--- 10430.java
+++ 10430.java
@@ -1,1 +1,21 @@
 // source code
+import java.util.Scanner;
+
+//백준 10430번
+public class Example02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);  // Scanner 객체 생성
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		System.out.println((A + B) % C);
+		System.out.println(((A % C) + (B%C)) % C);
+		System.out.println((A * B) % C);
+		System.out.println(((A % C) * (B % C)) % C);
+
+	}
+
+}
