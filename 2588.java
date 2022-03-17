664f4ad3a9d4b897934dcd94d767e3b2e9dc0776
이신우 <51151313+steven8602@users.noreply.github.com>
2022-03-17 20:03:07 +0900

    Update 2588.java

--- 2588.java
+++ 2588.java
@@ -6,15 +6,15 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);  // Scanner 객체 생성
 		
-		int a = sc.nextInt(); // 첫번 째 수 입력 받기
-		int b = sc.nextInt(); //두번 째 수 입력 받기
+		int a = sc.nextInt(); 
+		int b = sc.nextInt();
         
 		sc.close(); // Scanner 닫기
  
-		System.out.println(a*(b%10)); //***중 첫째 짜리 수 구하기
-		System.out.println(a*(b%100/10)); //***중 둘 짜리 수 구하기
-		System.out.println(a*(b/100)); //***중 셋째 짜리 수 구하기
-		System.out.println(a*b);  
+		System.out.println(a*(b%10)); //일의 자리 수 구하기
+		System.out.println(a*(b%100/10)); //십의 자리 수 구하기
+		System.out.println(a*(b/100)); //백의 자리 수 구하기
+		System.out.println(a*b); 
 
 	}
 
